9279ca58658f2b768839496c3e4b5dd329e630d4
gts250gamer101 <30349915+gts250gamer101@users.noreply.github.com>
2017-07-21 10:18:42 -0500

    Add files via upload

---
+++ Python System Information.py
@@ -0,0 +1,9 @@
+import platform
+print("CPU:")
+print (platform.machine())
+print("\n")
+print("Platform Version:")
+print (platform.version())
+print("\n")
+print ("System Platform:")
+print (platform.platform())
